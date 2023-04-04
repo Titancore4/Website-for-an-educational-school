8463ae959eaf1a539fa1f9afabead2ede70f4a3f
Jatin <hardevrag@gmail.com>
2023-04-04 11:14:59 +0530

    Adding the last three missing scholar images

--- src/scholars.js
+++ src/scholars.js
@@ -1,58 +1,60 @@
+import image1 from "./assets/scholars/me (2)-modified.png"
 import image2 from "./assets/scholars/Rajdeep-kaur-7.0.png"
 import image3 from "./assets/scholars/Sukhman-Sood-modified-transformed.png"
 import image4 from "./assets/scholars/simranjit-kaur-6.5.png"
 import image5 from "./assets/scholars/Gaurav-Gangar-modified-transformed.png"
-import image6 from "./assets/scholars/Rajdeep-kaur-7.0.png"
+import image6 from "./assets/scholars/Rajwinder-kaur-6.0.png"
 import image7 from "./assets/scholars/Manjinder-Singh-modified-transformed.png"
+import image8 from "./assets/scholars/Dilpreet-Singh_page-0001-modified (1) (1).png"
 
 const Scholars = [
     {
         id:1,
-        image: image2,
+        image: image1,
         name: "Jatin Rai",
-        score: 7.5
+        score: "7.5"
     },
     {
         id: 2,
         image: image2,
         name: "Rajdeep Kaur",
-        score: 7.0
+        score: "7.0"
     },
     {
         id: 3,
         image: image3,
         name: "Sukhman Sood",
-        score: 7.0
+        score: "7.0"
     },
     {
         id: 4,
         image: image4,
         name: "Simranjit Kaur",
-        score: 6.5
+        score: "6.5"
     },
     {
         id: 5,
         image: image5,
         name: "Gaurav Gangar",
-        score: 6.5
+        score: "6.5"
     },
     {
         id: 6,
         image: image6,
         name: "Rajwinder Kaur",
-        score: 6.0
+        score: "6.0"
     },
     {
         id: 7,
         image: image7,
         name: "Manjinder Singh",
-        score: 6.0
+        score: "6.0"
     },
     {
         id: 8,
-        image: image7,
+        image: image8,
         name: "Dilpreet Singh",
-        score: 6.0
+        score: "6.0"
     }
 ]
 
